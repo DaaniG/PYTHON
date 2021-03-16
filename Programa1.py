6ce8bf5a9c9d55cc1f6ef8031793d6f6c7a0f548
DaniG <75398496+DaaniG@users.noreply.github.com>
2021-03-16 23:34:46 +0100

    Add files via upload

---
+++ Programa1.py
@@ -0,0 +1,10 @@
+print("Esto es una suma")
+numero1 = 3
+numero2 = 2
+resultado = numero1 + numero2
+resultado = str(resultado)
+print("Si sumo", numero1, "mas", numero2, "es igual a", resultado)
+
+for(i == 5 , i < 10,  i++{
+    print(i)
+    }
